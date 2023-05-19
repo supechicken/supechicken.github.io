43f9688ed41fd3419ef121e341dc042da7928e74
supechicken <supechicken666@gmail.com>
2023-05-19 14:01:12 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-window.ontouchmove = window.onwheel = () => {
-  mainPage.classList.add('goaway');
-  endPage.style.display = 'flex';
+let lock = false,
+    touchPos = 0;
 
-  setInterval(() => {
-    mainPage.style.display = 'none';
-    endPage.classList.add('show');
-  }, 800);
+const mainContainer = document.getElementById('mainContainer');
+
+mainContainer.ontouchstart = e => {
+  touchPos = e.changedTouches[0].clientY;
+}
+
+mainContainer.onwheel = mainContainer.ontouchmove = e => {
+  e.preventDefault();
+
+  if (!lock) {
+    lock = true
+
+    const isScrollDown = (e.deltaY ? e.deltaY : touchPos - e.changedTouches[0].clientY) > 0,
+          containerHeight = mainContainer.offsetHeight,
+          targetPos = Math.min(Math.max(isScrollDown ? mainContainer.scrollTop + containerHeight : mainContainer.scrollTop - containerHeight, 0), containerHeight);
+
+    console.log(targetPos)
+    mainContainer.scrollTo({ top: targetPos, behavior: 'smooth' });
+
+    const interval = setInterval(() => {
+      if (Math.round(mainContainer.scrollTop - targetPos)) {
+        clearInterval(interval);
+        setTimeout(() => {
+          lock = false;
+          console.log('unlock');
+        }, 100);
+      }
+    }, 10);
+  }
 }
